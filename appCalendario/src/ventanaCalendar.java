a118c8dbc086375b4bf2d9512a3c3e2f477c5029
Oscar Bautista <oscar04262000@gmail.com>
2020-11-19 22:14:42 -0600

    Agregamos un menu a nuestro programa y un item para agregar una nota en la tabla

--- appCalendario/src/ventanaCalendar.java
+++ appCalendario/src/ventanaCalendar.java
@@ -1,11 +1,30 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.JScrollPane;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
 import java.awt.Color;
+import java.awt.event.ActionListener;
+
 
 public class ventanaCalendar extends JFrame{
 	
 	private JPanel panel1;
+	private JTable tbSemana;
+	private DefaultTableModel modelo;
+	private JScrollPane barra;
 	
+	private JMenuBar barraMenu;
+	private JMenu menuArchivo;
+	private JMenuItem menuItemNuevo;
+	private JPopupMenu menuEmergente;
+	
+	//Caracteristicas
 	public ventanaCalendar(){
 		setTitle("Calendario");
 		setSize(500,500);
@@ -13,12 +32,62 @@ public class ventanaCalendar extends JFrame{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		componentes();
+		tablaSemana();
+		menu();
 	}
 	
+	//crearPanel
 	private void componentes(){
 		panel1 = new JPanel();
 		panel1.setLayout(null);
 		panel1.setBackground(Color.GREEN);
 		this.getContentPane().add(panel1);
+	}
+	
+	//Crear Tabla que muestre la semana
+	private void tablaSemana(){
+		
+		tbSemana = new JTable();
+		String[] dias = {"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
+		Object datos[][] = {};
+		modelo = new DefaultTableModel(datos,dias){
+		};
+		
+		tbSemana.setModel(modelo);
+		tbSemana.getColumnModel().getColumn(0).setMaxWidth(64);
+		tbSemana.getColumnModel().getColumn(1).setMaxWidth(64);
+		tbSemana.getColumnModel().getColumn(2).setMaxWidth(64);
+		tbSemana.getColumnModel().getColumn(3).setMaxWidth(64);
+		tbSemana.getColumnModel().getColumn(4).setMaxWidth(64);
+		tbSemana.getColumnModel().getColumn(5).setMaxWidth(64);
+		tbSemana.getColumnModel().getColumn(6).setMaxWidth(64);
+		
+		barra = new JScrollPane();
+		barra.setBounds(15,60,450,300);
+		panel1.add(barra);
+		panel1.add(tbSemana);
+		barra.setViewportView(tbSemana);
+	}
+	
+	//Crear menu
+	private void menu(){
+		
+		barraMenu = new JMenuBar();
+		menuArchivo = new JMenu();
+		menuItemNuevo = new JMenuItem();	
+		menuEmergente = new JPopupMenu();
+		
+		//Crear los items
+		menuItemNuevo.setText("Nueva nota");
+		menuArchivo.add(menuItemNuevo);
+		
+		//Agregamos los menus de la barra de Menu
+		menuArchivo.setText("Archivo");
+		barraMenu.add(menuArchivo);
+		setJMenuBar(barraMenu);
+		
+		//Agregamos el menu en el panel
+		panel1.setComponentPopupMenu(menuEmergente);
+		
 	}
 }
